70c73786a22d44df12f685d0529beda018d5bbc6
pochinoritaro <pochimoritaro@gmail.com>
2023-07-14 20:28:17 +0900

    change card.json

--- card/card.py
+++ card/card.py
@@ -1,135 +1,52 @@
-"""
-caption
-
-Args
-
-"""
-import random
+from json import load
 from collections import defaultdict
+from random import randrange, sample, choice
+import copy
 
-class card:
-    def __init__(self, cards, member, given):
-        self.cards = cards
-        self.member = member
-        if given == 0:
-            self.ALL_FLAG = True
-            self.given, mod = divmod(len(cards), len(member))
-            self.mod = mod
-            #print(self.mod)
+class Game:
+    def __init__(self, choice_rule):
+        with open('config\cards.json') as cards:
+            self.deck = load(cards)
+
+        with open('config\members.json') as members:
+            self.players = load(members)
+
+        with open('config\game_rules.json') as rule:
+            self.rule = load(rule)
+
+        if choice_rule in list(self.rule.keys()):
+            self.rules = self.rule[choice_rule]
         else:
-            self.ALL_FLAG = False
-            self.given = given
-            #print(self.given)
+            print(list(self.rules.keys()))
 
-    def show_hand(self, hand, show_player):
-        """
-        Args:
+    def setting(self):
+        return list(self.rules.values())
 
-        Returns:
-            
-        Raises:
+    def start(self):
+        hand, draw, min_player, max_player, clockwise = self.setting()
+        print(hand)
+        self.throw(hand)
 
-        Yields:
-            
-        Example:
-            draw = Cards.draw(player_hands, member[0], 26)
-        """
-        return hand[show_player]
+    def throw(self, hand):
+        suit_and_number = defaultdict(list)
+        player_hand = defaultdict(dict)
 
-    def show_deck(self):
-        """
-        Args:
+        for i in range(0, hand):
+            for mem_cnt in self.players:
+                choice_suit = choice(list(self.deck.keys()))
 
-        Returns:
-            
-        Raises:
+                choose_num = self.deck[choice_suit].pop(randrange(0, len(self.deck[choice_suit])))
+                suit_and_number[choice_suit].append(choose_num)
 
-        Yields:
-            
-        Example:
-            draw = Cards.draw(player_hands, member[0], 26)
-        """
-        return self.cards
+                if choice_suit in player_hand[mem_cnt['name']]:
+                    player_hand[mem_cnt['name']][choice_suit].append(choose_num)
+                    
+                else:
+                    player_hand[mem_cnt['name']].update(dict(suit_and_number))
+                suit_and_number.clear()
 
-    def check_deck(func):
-        """
-        Args:
+        print(player_hand)
 
-        Returns:
-            
-        Raises:
-
-        Yields:
-            
-        Example:
-            draw = Cards.draw(player_hands, member[0], 26)
-        """
-        def wrapper(*args, **kwargs):
-            try:
-                return func(*args, **kwargs)
-            except:
-                print("ドローできるカードがありません。")
-        return wrapper
-
-    def partition(self) -> object :
-        """
-        Args:
-            None
-        Returns:
-            object(dict)
-        Raises:
-            None
-        Yields:
-            プレイヤーと初期ドロー数を設定して
-        Example:
-            draw = Cards.draw(player_hands, member[0], 26)
-        """
-        deck = self.cards
-        mem_hand = defaultdict(list)
-        for count in range(0, self.given):
-            for member_cards in self.member:
-                hand = []
-                take = deck.pop(random.randrange(0, len(deck)))
-                hand.append(take)
-                mem_hand[member_cards].append(take)
-            if self.ALL_FLAG:
-                for count in range(1, self.mod + 1):
-                    take = deck.pop(random.randrange(0, count))
-                    for mod_cards in random.sample(self.member, self.mod):
-                        mem_hand[mod_cards].append(take)
-                        self.mod -= 1
-        return dict(mem_hand)
-
-    @check_deck
-    def draw(self, add_card_dict: dict, draw_player, draw_count: int):
-        """
-        Args:
-
-        Returns:
-            
-        Raises:
-
-        Yields:
-            
-        Example:
-            draw = Cards.draw(player_hands, member[0], 26)
-        """
-        for count in range(0, draw_count):
-            hand = []
-            take = self.cards.pop(random.randrange(0, len(self.cards)))
-            hand.append(take)
-            add_card_dict[draw_player].append(take)
-        return dict(add_card_dict)
-
-if __name__ == "__main__":
-    cards = [
-	's1','s2','s3','s4','s5','s6','s7','s8','s9','s10','s11','s12','s13',
-	'c1','c2','c3','c4','c5','c6','c7','c8','c9','c10','c11','c12','c13',
-	'h1','h2','h3','h4','h5','h6','h7','h8','h9','h10','h11','h12','h13',
-	'd1','d2','d3','d4','d5','d6','d7','d8','d9','d10','d11','d12','d13'
-	]
-    member = ['a', 'b', 'c', 'd', 'e']
-    given = 0
-    CardIns = card(cards, member, given)
-
-    print(CardIns.partition())
+if __name__ == '__main__':
+    a = Game('black_jack')
+    s = a.start()
